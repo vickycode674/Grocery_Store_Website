f2420b4b94e06de4241b5e6935aa56769cbfb84b
MADHUKUMARAP <124053300+madhukumarap@users.noreply.github.com>
2023-12-27 12:58:28 +0530

    Add files via upload
    contact page

---
+++ src/Components/Contact/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Bannar from './banner';
+import Contact from "./contactInfo"
+import JustSayHello from './sayHello';
+const main = () => {
+  return (
+    <div>
+        <Bannar/>
+        <Contact/>
+        <JustSayHello/>
+    </div>
+  )
+}
+
+export default main
