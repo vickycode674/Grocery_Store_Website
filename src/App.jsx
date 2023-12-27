3e221a832805d8678938639ac66b113ba880ccc3
chviveksai <chvivek674@gmail.com>
2023-12-27 18:19:44 +0530

    Update About page: - Change "Our happy customer" to "Our happy customers" - Change "100% Sucure Payment" to "100% Secure Payment" - Change "We ensure your money is save" to "We ensure your money is safe" - Change "Free Shipping" to "Free Shipping" - Change "Free shipping with discount" to "Free shipping with discount" (generated by blackbox.ai )

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from './Components/About/About';
-// import Dashboard from "./Components/Dashboard/Dashboard";
-// import Settings from "./Components/Dashboard/Settings/Settings";
-// import OrderDetails from "./Components/Dashboard/Order/OrderDetails";
-// import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Settings from "./Components/Dashboard/Settings/Settings";
+import OrderDetails from "./Components/Dashboard/Order/OrderDetails";
+import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
 
 const App = () => {
   return (
     <div>
-    <About/>
-      {/* <Routes>
+    {/* <About/> */}
+      <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/setting" element={<Settings />} />
         <Route path="/order-details" element={<OrderDetails />} />
         <Route path="/order-history" element={<OrderHistory />} />
-      </Routes> */}
+        <Route path="/about" element={<About />} />
+
+      </Routes>
 
     </div>
   );
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,9 +3,15 @@ import About1 from "./Images/About1.png";
 import BG from "./Images/BG.png";
 import About2 from "./Images/About2.png";
 import leaf2 from "./Images/leaf2.png";
+import delivery from "./Images/delivery-truck 1.png";
+import Headphones from "./Images/Headphones 1.png";
+import Package from "./Images/package.png";
+import Shoppingbag from "./Images/shopping-bag.png";
+import stars from "./Images/stars 1.png";
+
 import { WeDelvieredYou } from "./WeDeliver";
 // // import { Box } from "./Box/Box1";
-import {Team} from "./Team";
+import { Team } from "./Team";
 import { Testimonial1 } from "./Testimonials/Testimonial1";
 const About = () => {
     return (
@@ -40,41 +46,112 @@ const About = () => {
                     </p>
 
                     <div className="grid-cols-2 ">
-
-                        <div className="inline-flex flex-wrap gap-[1px]  bg-gray-scalewhite relative left-[48%]  mt-[17%] ">
-                            <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
-                                <img className="relative flex-[0_0_auto]" alt="Icon" src={leaf2} />
-                            </div>
-                            <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
-                                <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
-                                    100% Organic food
+                        <div>
+                            <div className="inline-flex flex-wrap gap-[1px]  bg-gray-scalewhite relative left-[48%]  mt-[17%] ">
+                                <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                    <img className="relative flex-[0_0_auto]" alt="Icon" src={leaf2} />
                                 </div>
-                                <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
-                                    100% healthy &amp; Fresh food.
-                                </p>
+                                <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                    <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                        100% Organic food
+                                    </div>
+                                    <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                        100% healthy &amp; Fresh food.
+                                    </p>
+                                </div>
                             </div>
 
-                            <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
-                                <img className="relative flex-[0_0_auto]" alt="Icon" src={leaf2} />
-                            </div>
-                            <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
-                                <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
-                                    100% Organic food
+
+                            <div className="relative mt-[-112px] inline-flex left-[55%] bg-white">
+                                <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                    <img className="relative flex-[0_0_auto]" alt="Icon" src={Headphones} />
                                 </div>
-                                <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
-                                    100% healthy &amp; Fresh food.
-                                </p>
+                                <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                    <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                        Great Support 24/7
+                                    </div>
+                                    <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                        Instant access to Contact
+                                    </p>
+                                </div>
                             </div>
                         </div>
+
+
+                        <div className="flex-col relative mt-[-240px]" >
+                            <div>
+                                <div className="inline-flex flex-wrap gap-[1px]  bg-gray-scalewhite relative left-[48%]  mt-[17%] ">
+                                    <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                        <img className="relative flex-[0_0_auto]" alt="Icon" src={stars} />
+                                    </div>
+                                    <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                        <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                            Customer Feedback
+                                        </div>
+                                        <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                            Our happy customer
+                                        </p>
+                                    </div>
+                                </div>
+
+
+                                <div className="relative mt-[-112px] inline-flex left-[55%] bg-white">
+                                    <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                        <img className="relative flex-[0_0_auto]" alt="Icon" src={Shoppingbag} />
+                                    </div>
+                                    <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                        <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                            100% Sucure Payment
+                                        </div>
+                                        <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                            We ensure your money is save                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex-col relative mt-[-240px]" >
+                            <div>
+                                <div className="inline-flex flex-wrap gap-[1px]  bg-gray-scalewhite relative left-[48%]  mt-[17%] ">
+                                    <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                        <img className="relative flex-[0_0_auto]" alt="Icon" src={delivery} />
+                                    </div>
+                                    <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                        <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                            Free Shipping                                        </div>
+                                        <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                            Free shipping with discount                                        </p>
+                                    </div>
+                                </div>
+
+
+                                <div className="relative mt-[-112px] inline-flex left-[55%] bg-white">
+                                    <div className=" inline-flex flex-col items-start p-[20px] relative bg-[#00b2061a] rounded-[1000px]">
+                                        <img className="relative flex-[0_0_auto]" alt="Icon" src={Package} />
+                                    </div>
+                                    <div className="inline-flex flex-col items-start justify-center gap-[8px] relative flex-[0_0_auto]">
+                                        <div className="relative w-[224px] mt-[-1.00px] font-body-large-body-large-500 font-[number:var(--body-large-body-large-500-font-weight)] text-gray-scalegray-900 text-[length:var(--body-large-body-large-500-font-size)] tracking-[var(--body-large-body-large-500-letter-spacing)] leading-[var(--body-large-body-large-500-line-height)] [font-style:var(--body-large-body-large-500-font-style)]">
+                                            100% Organic food
+                                        </div>
+                                        <p className="relative w-[224px] font-body-small-body-small-400 font-[number:var(--body-small-body-small-400-font-weight)] text-gray-scalegray-500 text-[length:var(--body-small-body-small-400-font-size)] tracking-[var(--body-small-body-small-400-letter-spacing)] leading-[var(--body-small-body-small-400-line-height)] [font-style:var(--body-small-body-small-400-font-style)]">
+                                            100% healthy &amp; Fresh food.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
 
 
 
                         {/* part 3 delivery agent */}
                     </div>
                 </div>
-                 <WeDelvieredYou/>
-               <Team/>
-            <Testimonial1/>  
+                <WeDelvieredYou />
+                <Team />
+                <Testimonial1 />
 
             </div>
         </div>
