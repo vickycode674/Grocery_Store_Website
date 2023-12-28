7748e4ef89661993bd42a5e79af4ab69361d6220
chviveksai <chvivek674@gmail.com>
2023-12-28 17:21:48 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-// import { Route, Routes } from "react-router-dom";
 
-// import About from "./Components/About/About";e
-// import Dashboard from "./Components/Dashboard/Dashboard";
-// import Settings from "./Components/Dashboard/Settings/Settings";
-// import OrderDetails from "./Components/Dashboard/Order/OrderDetails";
-// import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
-// import { Route, Routes } from "react-router-dom";
-// import About from "./Components/About/About";
-// import Dashboard from "./Components/Dashboard/Dashboard";
-// import Settings from "./Components/Dashboard/Settings/Settings";
-// import OrderDetails from "./Components/Dashboard/Order/OrderDetails";
-// import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
-// import VegProducts from "./Components/Products/VegProducts";
-// import Home from "./Components/pages/Home";
-// import Main from './Components/Contact/Main'
-// import Header from './Components/Homepage/Header'
-// import SubscribeOurNewsletter from "./Components/Homepage/SubscribeOurNewsletter";
+import { Route, Routes } from "react-router-dom";
+import About from "./Components/About/About";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Settings from "./Components/Dashboard/Settings/Settings";
+import OrderDetails from "./Components/Dashboard/Order/OrderDetails";
+import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
+import VegProducts from "./Components/Products/VegProducts";
+import Home from "./Components/pages/Home";
+import Main from './Components/Contact/Main'
+import Header from './Components/Homepage/Header'
+import SubscribeOurNewsletter from "./Components/Homepage/SubscribeOurNewsletter";
 // import Shop1 from "./Components/Shop/MainShop/Shop1";
 import Footer from "./Components/Homepage/Footer";
 
@@ -23,7 +17,7 @@ const App = () => {
   return (
 
     <div>
-      {/* <Header/>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -34,7 +28,7 @@ const App = () => {
         <Route path="/order-details" element={<OrderDetails />} />
         <Route path="/order-history" element={<OrderHistory />} />
       </Routes>
-      <SubscribeOurNewsletter/> */}
+      <SubscribeOurNewsletter/>
       <Footer/>
     </div>
 
