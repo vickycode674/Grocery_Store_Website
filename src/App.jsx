26c38e2ff882be0ed5d570ef8b8c1657de8cffdd
MADHUKUMARAP <124053300+madhukumarap@users.noreply.github.com>
2024-01-03 14:32:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,21 @@ import OrderHistory from "./Components/Dashboard/Order/OrderHistory";
 import VegProducts from "./Components/Products/VegProducts";
 import Home from "./Components/pages/Home";
 import Main from './Components/Contact/Main'
-import Header from './Components/Homepage/Header'
+// import Header from './Components/Homepage/Header'
 import SubscribeOurNewsletter from "./Components/Homepage/SubscribeOurNewsletter";
 import SignIn from "./Components/SignIn/SignIn";
 import Shop1 from './Components/Shop/MainShop/Shop1'
-
-// import Footer from "./Components/Homepage/Footer";
-import Page from "./Components/Faq/Page"
-
-// import Footer from "./Components/Homepage/Group";
-import Footer from "./Components/Homepage/Footer";
-// import Page from "./Components/Faq/Page"
-
-
-// import Footer from "./Components/Homepage/Group";
-
 import CreateAccount from "./Components/CreateAccount/CreateAccount";
-import Bottam from "./Components/Homepage/Bottam"
 import Whishlist from "./Components/Wishlist/container"
 import ShoppingPage from "./Components/ShoppingCart/shoppingPage";
 import CheckOutpage from "./Components/Checkout/checkOutpage";
-// import Page from "./Components/Faq/Page"; 
-
+import Navbarrr from "./Components/home/navbar"
+import Footer from "./Components/home/footer"
 const App = () => {
   return (
-    <div>
-      <Header/>
+    <div >
+      <Navbarrr/>
+      {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/wishlist" element={<Whishlist />} />
@@ -48,12 +37,12 @@ const App = () => {
         <Route path="/setting" element={<Settings />} />
         <Route path="/order-details" element={<OrderDetails />} />
         <Route path="/order-history" element={<OrderHistory />} />
-        <Route path="/faq" element={<Page/>} />
+        {/* <Route path="/faq" element={<Page/>} /> */}
 
       </Routes>
       <SubscribeOurNewsletter/>
-       <Footer/> 
-      <Bottam/>
+      <Footer/>
+      {/* <Bottam/> */}
     </div>
   );
 };
